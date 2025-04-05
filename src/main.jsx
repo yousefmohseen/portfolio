5fbedc58dea808241db7232392ed589191468058
yousefmohseen <abuturaab12345@gmail.com>
2025-04-06 00:51:57 +0300

    update 1.4

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './pages/Root.jsx'
 import Home from './pages/Home.jsx'
 import Details from './pages/Details.jsx'
 
-const routes = createHashRouter([
+const routes = createBrowserRouter([
   {
     path: "/portfolio",
     element: <Root />,
